6391aa49b583dee45c6bd3975cf6ef479c0abb2b
Scott Williams <scott@swilliams.me>
2019-01-24 08:17:01 -0700

    Got post import working

--- content/blog/copying-the-current-page-from-safari/index.md
+++
@@ -1,16 +0,0 @@
----
-layout: post
-title: "Copying the Current Page From Safari"
-date: 2014-11-19T20:22:16.000Z
-comments: false
-categories: script
----
-If I'm preparing a bunch of links for a blog post or something, it'd speed things up if I could hit a button and have the computer do everything for me. Here's an [AppleScript](https://developer.apple.com/library/mac/documentation/AppleScript/Conceptual/AppleScriptLangGuide/introduction/ASLR_intro.html) to do just that. 
-
-```applescript
-tell application "Safari"	set theURL to URL of current tab of window 1	set theTitle to name of current tab of window 1	set the clipboard to "[" & theTitle & "](" & theURL & ")"end tell
-```
-
-That'll copy the URL and title of the current tab in Safari as a nicely formatted [Markdown](http://daringfireball.net/projects/markdown/) link. I tied that into a [Keyboard Maestro](http://www.keyboardmaestro.com/main/) shortcut, so that I can quickly make the link with ⌘⌥⌃C, then paste it into whichever document I'm working in.
-
-This'll probably work with Chrome too, but I don't know how much it supports AppleScript.
--- importer.js
+++ importer.js
@@ -1,10 +1,20 @@
 const fs = require('fs');
+const path = require('path');
+const rimraf = require('rimraf');
 
 // Move posts from Octopress /_posts into the gatsby project in their 
 // own folders. Something like /src/pages/2017-08-...
 
 const octoFolderPath = '/Users/swilliams/blog/source/_posts';
 const gatsbyFolderPath = './content/blog';
+
+const getDateComponents = (s) => {
+  const pattern = /^(\d{4}-\d{2}-\d{2})/;
+  const [dateStr] = pattern.exec(s);
+  if (!dateStr) { throw 'Couldnt parse date str'; }
+  const [year, month, day] = dateStr.split('-');
+  return { year, month, day };
+}
 
 const stripDateFromString = (s) => {
   // 2018-11-30-what-are-you-struggling-with.markdown
@@ -33,8 +43,22 @@ const getExtension = (s) => {
   return result[1];
 }
 
+const makeDir = (title, dateComponents) => {
+  const { year, month, day } = dateComponents;
+  let p = path.join(gatsbyFolderPath, year);
+  ensureFolderExists(p);
+  p = path.join(p, month);
+  ensureFolderExists(p);
+  p = path.join(p, day);
+  ensureFolderExists(p);
+  p = path.join(p, title);
+  ensureFolderExists(p);
+  return p;
+};
+
 const handleMarkdown = (f) => {
-  const fullPath = `${octoFolderPath}/${f}`;
+  const fullPath = path.join(octoFolderPath, f);
+  const dateComponents = getDateComponents(f);
   const postTitle = removeExtension(stripDateFromString(f));
   const postText = fs.readFileSync(fullPath, 'utf8');
   const updatedPostText = updateDateInPost(postText);
@@ -42,14 +66,15 @@ const handleMarkdown = (f) => {
     console.log('couldnt update date in ', postTitle);
     return; 
   }
-  fs.mkdirSync(`${gatsbyFolderPath}/${postTitle}`);
-  const writeFilePath = `${gatsbyFolderPath}/${postTitle}/index.md`;
+  const folderPath = makeDir(postTitle, dateComponents);
+  const writeFilePath = path.join(folderPath, 'index.md');
   fs.writeFileSync(writeFilePath, updatedPostText);
 
 };
 
 const handleHtml = (f) => {
   const fullPath = `${octoFolderPath}/${f}`;
+  const dateComponents = getDateComponents(f);
   const postTitle = removeExtension(stripDateFromString(f));
   const postText = fs.readFileSync(fullPath, 'utf8');
   const updatedPostText = updateDateInPost(postText);
@@ -57,34 +82,50 @@ const handleHtml = (f) => {
     console.log('couldnt update date in ', postTitle);
     return; 
   }
-  fs.mkdirSync(`${gatsbyFolderPath}/${postTitle}`);
-  const writeFilePath = `${gatsbyFolderPath}/${postTitle}/index.html`;
+  const folderPath = makeDir(postTitle, dateComponents);
+  const writeFilePath = path.join(folderPath, 'index.html');
   fs.writeFileSync(writeFilePath, updatedPostText);
 };
 
-// get all the files in _posts
-const files = fs.readdirSync(octoFolderPath);
-files.forEach((f) => {
-  if (f === '.DS_Store') { return; }
-  // console.log('reading:', fullPath);
-  const ext = getExtension(f);
-  if (ext === 'markdown' || ext === 'md') {
-    handleMarkdown(f);
-  } else if (ext === 'html') {
-    handleHtml(f);
-  } else {
-    console.log('unknown filetype', f);
+const importPosts = () => {
+  const files = fs.readdirSync(octoFolderPath);
+  files.forEach((f) => {
+    if (f === '.DS_Store') { return; }
+    // console.log('reading:', fullPath);
+    const ext = getExtension(f);
+    if (ext === 'markdown' || ext === 'md') {
+      handleMarkdown(f);
+    } else if (ext === 'html') {
+      handleHtml(f);
+    } else {
+      console.log('unknown filetype', f);
+    }
+
+    // console.log('writing', writeFilePath);
+
+  });
+
+};
+
+const ensureFolderExists = (p) => {
+  if (!fs.existsSync(p)) {
+    fs.mkdirSync(p);
   }
+};
 
-  // console.log('writing', writeFilePath);
+const removeGatsbyPosts = () => {
+  const files = fs.readdirSync(gatsbyFolderPath);
+  files.forEach((f) => {
+    const p = path.join(gatsbyFolderPath, f);
+    if (fs.lstatSync(p).isDirectory()) {
+      rimraf.sync(p);
+    }
+  });
+}
 
-});
-// strip the date from the filename
-// open the file, parse the date format that looks like this:
-//
-// date: 2018-12-11 11:11:51 -0700
-//
-// to:
-// 
-// date: '2015-05-01T22:12:03.284Z'
-// console.log(files);
+const run = () => {
+  removeGatsbyPosts();
+  // importPosts();
+}
+
+run();
