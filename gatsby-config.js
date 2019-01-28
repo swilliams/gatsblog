3852049db3452e490184c5cd1a7ae0a6a6ab03bd
Scott Williams <scott@swilliams.me>
2019-01-28 14:20:57 -0700

    Updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,6 +40,7 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
+          'gatsby-remark-autolink-headers',
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
@@ -51,7 +52,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-57339923-1`,
       },
     },
     `gatsby-plugin-feed`,
