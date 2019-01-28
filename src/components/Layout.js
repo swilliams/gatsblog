b9610a0522d55e1ee524e4b148fb78b425d413ef
Scott Williams <scott@swilliams.me>
2019-01-27 18:11:45 -0700

    Updates fonts and stuff

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -34,7 +34,7 @@ class Layout extends React.Component {
       header = (
         <h3
           style={{
-            fontFamily: `Montserrat, sans-serif`,
+            fontFamily: `Roboto Slab, sans-serif`,
             marginTop: 0,
           }}
         >
