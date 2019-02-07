95c582f1ae1b3da67906524783cb27632a9b3fa1
Scott Williams <scott@swilliams.me>
2019-02-07 08:08:17 -0700

    Adds littlefoot for footnotes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,14 @@
 require("prismjs/themes/prism-dark.css");
+require('littlefoot/dist/littlefoot.css');
+
 
 // custom typefaces
 import 'typeface-roboto-slab'
 import 'typeface-open-sans'
+
+// plugins
+import littlefoot from 'littlefoot';
+
+export function onRouteUpdate() {
+  littlefoot() // Pass any littlefoot settings here.
+}
